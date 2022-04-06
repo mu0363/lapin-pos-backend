baecca2064fbd80e57e9bce8ddb13cbf8fde52bc
mu0363 <dde036@gmail.com>
2022-04-06 09:14:28 +0900

    :bug: bug: remove unused line

--- src/session/models/session.model.ts
+++ src/session/models/session.model.ts
@@ -1,11 +1,9 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
-import { Customer } from '@prisma/client';
 
 @ObjectType()
 export class Session {
   @Field(() => Int)
   id: number;
-  customer: Customer[];
   customerId: string;
   castId: string;
   planId: string;
