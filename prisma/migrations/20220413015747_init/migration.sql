4c3606d93459c28b691abf4c22886098988f6bdb
mu0363 <dde036@gmail.com>
2022-04-13 10:58:12 +0900

    :bulb: fix: migrate

--- prisma/migrations/20220413015747_init/migration.sql
+++ prisma/migrations/20220413015747_init/migration.sql
@@ -25,8 +25,10 @@ CREATE TABLE `items` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `categoryId` INTEGER NOT NULL,
+    `userId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `items_item_name_key`(`item_name`),
+    UNIQUE INDEX `items_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -34,11 +36,12 @@ CREATE TABLE `items` (
 CREATE TABLE `categorys` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `category_name` VARCHAR(255) NOT NULL,
-    `category_icon` VARCHAR(255) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `categorys_category_name_key`(`category_name`),
+    UNIQUE INDEX `categorys_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -48,12 +51,14 @@ CREATE TABLE `casts` (
     `cast_name` VARCHAR(255) NOT NULL,
     `cast_kana` VARCHAR(255) NOT NULL,
     `cast_icon` VARCHAR(255) NOT NULL,
+    `last_work_date` DATETIME(3) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `userId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `casts_cast_name_key`(`cast_name`),
     UNIQUE INDEX `casts_cast_kana_key`(`cast_kana`),
+    UNIQUE INDEX `casts_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -64,8 +69,10 @@ CREATE TABLE `plans` (
     `price` INTEGER NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `plans_plan_name_key`(`plan_name`),
+    UNIQUE INDEX `plans_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -76,13 +83,15 @@ CREATE TABLE `customers` (
     `customer_name` VARCHAR(255) NOT NULL,
     `birth_day` DATETIME(3) NULL,
     `receipt` VARCHAR(255) NULL,
+    `last_visit_date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
-    `castId` INTEGER NOT NULL,
+    `castId` INTEGER NULL,
     `userId` VARCHAR(191) NULL,
 
     UNIQUE INDEX `customers_customer_kana_key`(`customer_kana`),
     UNIQUE INDEX `customers_customer_name_key`(`customer_name`),
+    UNIQUE INDEX `customers_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -94,8 +103,9 @@ CREATE TABLE `sessions` (
     `customer_id` INTEGER NOT NULL,
     `cast_id` INTEGER NOT NULL,
     `plan_id` INTEGER NOT NULL,
-    `userId` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
 
+    UNIQUE INDEX `sessions_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -105,26 +115,69 @@ CREATE TABLE `orders` (
     `item_id` INTEGER NOT NULL,
     `count` INTEGER NOT NULL,
     `total` INTEGER NOT NULL,
-    `sessionId` INTEGER NULL,
+    `sessionId` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
 
-    UNIQUE INDEX `orders_item_id_key`(`item_id`),
+    UNIQUE INDEX `orders_id_userId_key`(`id`, `userId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `liquor_store` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `liquor_store_name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+    `staff` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `liquor_store_id_userId_key`(`id`, `userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `purchases` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(191) NOT NULL,
+    `liquor_store_name` VARCHAR(191) NOT NULL,
+    `price` INTEGER NOT NULL,
+    `count` INTEGER NOT NULL,
+    `totalPrice` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `userId` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `purchases_id_userId_key`(`id`, `userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `items` ADD CONSTRAINT `items_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `items` ADD CONSTRAINT `items_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `categorys`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `categorys` ADD CONSTRAINT `categorys_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `casts` ADD CONSTRAINT `casts_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `plans` ADD CONSTRAINT `plans_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `customers` ADD CONSTRAINT `customers_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `customers` ADD CONSTRAINT `customers_castId_fkey` FOREIGN KEY (`castId`) REFERENCES `casts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `customers` ADD CONSTRAINT `customers_castId_fkey` FOREIGN KEY (`castId`) REFERENCES `casts`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `sessions` ADD CONSTRAINT `sessions_cast_id_fkey` FOREIGN KEY (`cast_id`) REFERENCES `casts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -136,7 +189,16 @@ ALTER TABLE `sessions` ADD CONSTRAINT `sessions_plan_id_fkey` FOREIGN KEY (`plan
 ALTER TABLE `sessions` ADD CONSTRAINT `sessions_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE `orders` ADD CONSTRAINT `orders_item_id_fkey` FOREIGN KEY (`item_id`) REFERENCES `items`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `orders` ADD CONSTRAINT `orders_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `sessions`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `orders` ADD CONSTRAINT `orders_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `sessions`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `liquor_store` ADD CONSTRAINT `liquor_store_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `purchases` ADD CONSTRAINT `purchases_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
