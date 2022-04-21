a4b34b5ce7b4d75139a7c4c2cc642a405fae8b03
mu0363 <dde036@gmail.com>
2022-04-21 12:22:50 +0900

    add: デバッグ用の認証回避コードを追記

--- src/common/decorators/current-user-id.decorator.ts
+++ src/common/decorators/current-user-id.decorator.ts
@@ -5,6 +5,7 @@ export const GetCurrentUserId = createParamDecorator(
   (_data: string | undefined, context: ExecutionContext) => {
     const ctx = GqlExecutionContext.create(context);
     const request = ctx.getContext().req;
+    // return '874ca59d-2bf8-4674-b21f-7630d612e612';
     return request.user.id;
   },
 );
